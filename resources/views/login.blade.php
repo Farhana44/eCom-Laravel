1f058c05ef6a5532f17407af766b7865b58786df
Farhana <Farhanaabdullah365@gmail.com>
2023-03-19 21:31:53 +0530

    Done with Sessions

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,17 +8,18 @@
 	<div class="row">
 		<div class="col-sm-4 offset-4">
 
-			<form>
+			<form method="POST" action="/login">
+				@csrf
 			  <div class="mb-3">
 			    <label for="exampleInputEmail1" class="form-label">Email address</label>
-			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+			    <input type="email"  class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
 			  </div>
 			  <div class="mb-3">
 			    <label for="exampleInputPassword1" class="form-label">Password</label>
-			    <input type="password" class="form-control" id="exampleInputPassword1">
+			    <input type="password"  class="form-control" name="password" id="exampleInputPassword1">
 			  </div>
 			  
-			  <button type="submit" class="btn btn-primary">Submit</button>
+			  <button type="submit" class="btn btn-primary">Login</button>
 			</form>
 			
 		</div>
